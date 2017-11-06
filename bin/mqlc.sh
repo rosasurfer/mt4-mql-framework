4cdd6c69a90c456a23ebd57b5a7de64547872950
Peter Walther <pewa-github@rosasurfer.com>
2017-11-06 12:55:59 +0200

    add MQL compiler and shell script to call it

---
+++ bin/mqlc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+
+
+# find the MQL compiler
+SCRIPT_DIR=$(dirname "$(readlink -e "$0")")
+mqlc=
+[ -z "$mqlc" ] && [ -f "$SCRIPT_DIR/metalang.exe"   ] && mqlc="$SCRIPT_DIR/metalang.exe"
+[ -z "$mqlc" ] && command -v metalang >/dev/null      && mqlc="metalang" 
+[ -z "$mqlc" ] && [ -f "$SCRIPT_DIR/metaeditor.exe" ] && mqlc="$SCRIPT_DIR/metaeditor.exe"
+[ -z "$mqlc" ] && command -v metaeditor >/dev/null    && mqlc="metaeditor" 
+[ -z "$mqlc" ] && { echo "ERROR: MQL compiler not found."; exit 1; } 
+
+
+# call it
+"$mqlc" "$@"
